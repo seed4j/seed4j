080f6f2d501d5f48f273680be5b6c03fb7f7a802
Anthony Viard <anth.viard@gmail.com>
2021-11-26 16:23:17 +0100

    Move to Swagger 3 specs for API

--- src/main/java/tech/jhipster/light/generator/server/springboot/cache/caffeine/infrastructure/primary/CaffeineResource.java
+++ src/main/java/tech/jhipster/light/generator/server/springboot/cache/caffeine/infrastructure/primary/CaffeineResource.java
@@ -1,9 +1,9 @@
 package tech.jhipster.light.generator.server.springboot.cache.caffeine.infrastructure.primary;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,7 @@ import tech.jhipster.light.generator.server.springboot.cache.caffeine.applicatio
 
 @RestController
 @RequestMapping("/api/servers/spring-boot/cache")
-@Api(tags = "Spring Boot")
+@Tag(name = "Spring Boot")
 class CaffeineResource {
 
   private final CaffeineApplicationService caffeineApplicationService;
@@ -23,8 +23,8 @@ class CaffeineResource {
     this.caffeineApplicationService = caffeineApplicationService;
   }
 
-  @ApiOperation("Init the Spring cache configuration")
-  @ApiResponses({ @ApiResponse(code = 500, message = "An error occurred while initializing project") })
+  @Operation(summary = "Init the Spring cache configuration")
+  @ApiResponses({ @ApiResponse(responseCode = "500", description = "An error occurred while initializing project") })
   @PostMapping("/init")
   public void initSpringCache(@RequestBody ProjectDTO projectDTO) {
     Project project = ProjectDTO.toProject(projectDTO);

93839ea8bfe0bc4be0af97233c9e8fc5c9fa2aec
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-07 23:34:50 +0100

    Try to fix write method for Windows

--- src/main/java/tech/jhipster/lite/generator/project/infrastructure/secondary/ProjectLocalRepository.java
+++ src/main/java/tech/jhipster/lite/generator/project/infrastructure/secondary/ProjectLocalRepository.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import tech.jhipster.lite.common.domain.FileUtils;
+import tech.jhipster.lite.common.domain.WordUtils;
 import tech.jhipster.lite.error.domain.Assert;
 import tech.jhipster.lite.error.domain.GeneratorException;
 import tech.jhipster.lite.generator.project.domain.Project;
@@ -126,7 +127,7 @@ public class ProjectLocalRepository implements ProjectRepository {
 
     try {
       FileUtils.createFolder(projectDestination);
-      Files.write(getPathOf(projectDestinationFilename), text.getBytes());
+      Files.write(getPathOf(projectDestinationFilename), text.replace(WordUtils.LF, project.getEndOfLine()).getBytes());
     } catch (IOException e) {
       throw new GeneratorException(getErrorWritingMessage(projectDestinationFilename));
     }

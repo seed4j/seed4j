7374eac7746273209c456f7e6cd9d5b7f87219ce
Vladislav Sokolov <vladislav.sokolov@hotmail.fr>
2022-12-12 23:26:56 +0100

    fix code smell

--- src/main/java/tech/jhipster/lite/generator/server/springboot/dbmigration/cassandra/domain/CassandraMigrationModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/dbmigration/cassandra/domain/CassandraMigrationModuleFactory.java
@@ -11,9 +11,8 @@ import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
 public class CassandraMigrationModuleFactory {
 
   private static final JHipsterSource SOURCE = from("server/springboot/dbmigration/cassandra");
-
   private static final String DOCKER_COMPOSE_COMMAND = "docker compose -f src/main/docker/cassandra-migration.yml up -d";
-
+  private static final String CASSANDRA = "cassandra";
   private final DockerImages dockerImages;
 
   public CassandraMigrationModuleFactory(DockerImages dockerImages) {
@@ -26,12 +25,12 @@ public class CassandraMigrationModuleFactory {
     //@formatter:off
     return moduleBuilder(properties)
       .context()
-        .put("cassandraDockerImage", dockerImages.get("cassandra").fullName())
+        .put("cassandraDockerImage", dockerImages.get(CASSANDRA).fullName())
         .and()
       .documentation(documentationTitle("Cassandra Migration"), SOURCE.file("cassandra-migration.md"))
       .startupCommand(DOCKER_COMPOSE_COMMAND)
       .files()
-        .add(SOURCE.template("Cassandra-Migration.Dockerfile"), toSrcMainDocker().append("cassandra").append("Cassandra-Migration.Dockerfile"))
+        .add(SOURCE.template("Cassandra-Migration.Dockerfile"), toSrcMainDocker().append(CASSANDRA).append("Cassandra-Migration.Dockerfile"))
         .add(SOURCE.file("cassandra-migration.yml"), toSrcMainDocker().append("cassandra-migration.yml"))
         .add(SOURCE.file("autoMigrate.sh"), toSrcMainDockerScripts().append("autoMigrate.sh"))
         .add(SOURCE.file("execute-cql.sh"), toSrcMainDockerScripts().append("execute-cql.sh"))
@@ -47,6 +46,6 @@ public class CassandraMigrationModuleFactory {
   }
 
   private JHipsterDestination toSrcMainDockerScripts() {
-    return toSrcMainDocker().append("cassandra").append("scripts");
+    return toSrcMainDocker().append(CASSANDRA).append("scripts");
   }
 }

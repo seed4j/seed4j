57bceac4486f972ac98a0a3720b28833e3e99bba
Vladislav Sokolov <vladislav.sokolov@hotmail.fr>
2022-12-29 21:39:08 +0100

    improve quality of IT in cassandra module

--- src/test/resources/projects/cassandra/TestCassandraManager.java
+++ src/test/resources/projects/cassandra/TestCassandraManager.java
@@ -1,6 +1,8 @@
-package com.jhipster.test;
+package com.mycompany.myapp;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
@@ -9,8 +11,8 @@ import org.testcontainers.containers.CassandraContainer;
 
 class TestCassandraManager implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
-  private static final String KEYSPACE = "testKeyspace";
-  private static CassandraContainer cassandraContainer;
+  private static final String KEYSPACE = "jhipsterSampleApplication";
+  private static CassandraContainer<?> cassandraContainer;
   private static final Integer CONTAINER_STARTUP_TIMEOUT_MINUTES = 10;
 
   @Override
@@ -34,9 +36,24 @@ class TestCassandraManager implements ApplicationListener<ApplicationEnvironment
 
   private void registerEnvironmentVariables() {
     System.setProperty("TEST_CASSANDRA_PORT", String.valueOf(cassandraContainer.getMappedPort(CassandraContainer.CQL_PORT)));
-    System.setProperty("TEST_CASSANDRA_CONTACT_POINT", cassandraContainer.getHost());
+    System.setProperty("TEST_CASSANDRA_CONTACT_POINT", getContainerIpAddress());
     System.setProperty("TEST_CASSANDRA_DC", cassandraContainer.getLocalDatacenter());
     System.setProperty("TEST_CASSANDRA_KEYSPACE", KEYSPACE);
+  }
+
+  /**
+   *  For the driver contact point, an IP address should be used, and not "localhost" which can resolve to 2 addresses: ipv4 and ipv6.
+   *  If it happens, the driver creates 2 contact points, and the second one throws a WARN in logs.
+   *  Currently, cassandraContainer.getHost() returns "localhost", so it can't be used directly.
+   * @see <a href="https://docs.datastax.com/en/developer/java-driver/4.3/manual/core/configuration/reference/">...</a>
+   * @return cassandra container's ip address, or 127.0.0.1 as fallback
+   */
+  private String getContainerIpAddress() {
+    try {
+      return InetAddress.getByName(cassandraContainer.getHost()).getHostAddress();
+    } catch (UnknownHostException e) {
+      return "127.0.0.1";
+    }
   }
 
   private void createTestKeyspace(CqlSession session) {
@@ -54,7 +71,7 @@ class TestCassandraManager implements ApplicationListener<ApplicationEnvironment
 
   private void createCassandraContainer() {
     cassandraContainer =
-      (CassandraContainer) new CassandraContainer("4.0.7")
+      new CassandraContainer<>("cassandra:4.0.7")
         .withStartupTimeout(Duration.of(CONTAINER_STARTUP_TIMEOUT_MINUTES, ChronoUnit.MINUTES))
         .withExposedPorts(CassandraContainer.CQL_PORT);
   }

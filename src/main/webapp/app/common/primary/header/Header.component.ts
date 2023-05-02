b64249b2945d86e02beaea12f9bc2c16d00a1f2f
Yadong Zhang <zhyd007@gmail.com>
2023-05-02 07:59:59 +0000

    ci: sonar cloud.

--- src/main/webapp/app/common/primary/header/Header.component.ts
+++ src/main/webapp/app/common/primary/header/Header.component.ts
@@ -1,4 +1,6 @@
-import { defineComponent } from 'vue';
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { defineComponent, onMounted } from 'vue';
 import { IconVue } from '@/common/primary/icon';
 
 export default defineComponent({

71e17ab983cabcf5cef11a6534a4670db322d86b
Quentin <quentin.monmert@gmail.com>
2024-11-05 21:32:20 +0100

    Cypress: fusion of 2 two folders

--- src/test/webapp/component/cypress-config-9000.ts
+++ src/test/webapp/component/cypress-config-9000.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:7471',
+    baseUrl: 'http://localhost:9000',
     specPattern: 'src/test/webapp/component/integration/**/*.spec.ts',
     fixturesFolder: 'src/test/webapp/component/fixtures',
     supportFolder: 'src/test/webapp/component/support',
--- src/test/webapp/e2e/integration/Patch.spec.ts
+++
@@ -1,9 +0,0 @@
-import { dataSelector } from '../support/selector';
-
-describe('Patch', () => {
-  it('should display modules', () => {
-    cy.visit('/patches');
-
-    cy.get(dataSelector('modules-list')).should('be.visible');
-  });
-});

906be7358127975698c5c5f1099821a2806f33d7
Renan Franca <renan.andradefranca@gmail.com>
2025-02-14 11:50:18 -0300

    fix: sonar issue at ModuleRankStatistics.ts

--- src/main/webapp/app/module/domain/ModuleRankStatistics.ts
+++ src/main/webapp/app/module/domain/ModuleRankStatistics.ts
@@ -5,17 +5,18 @@ import { ModuleRank, RANKS } from '@/module/domain/landscape/ModuleRank';
 export type ModuleRankStatistics = ModuleRankCount[];
 
 export const toModuleRankStatistics = (landscape: Landscape): ModuleRankStatistics => {
-  const rankCounts = new Map<ModuleRank, number>(RANKS.map(rank => [rank, 0]));
-
-  landscape
+  const rankCounts = landscape
     .standaloneLevels()
     .flatMap(level => level.elements)
     .flatMap(element => element.allModules())
-    .reduce((counts, module) => {
-      const currentCount = counts.get(module.rank()) || 0;
-      counts.set(module.rank(), currentCount + 1);
-      return counts;
-    }, rankCounts);
+    .reduce(
+      (counts, module) => {
+        const currentCount = counts.get(module.rank()) || 0;
+        counts.set(module.rank(), currentCount + 1);
+        return counts;
+      },
+      new Map<ModuleRank, number>(RANKS.map(rank => [rank, 0])),
+    );
 
   return RANKS.map(rank => ({
     rank,

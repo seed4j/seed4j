03d6009078fa42944a533320dc2e25a01508674c
Aurélien Mino <aurelien.mino@gmail.com>
2025-11-29 15:31:37 +0100

    feat(jacoco): exclude generated JPA metamodel from coverage check

--- src/main/java/com/seed4j/generator/server/javatool/jacoco/domain/JacocoModuleFactory.java
+++ src/main/java/com/seed4j/generator/server/javatool/jacoco/domain/JacocoModuleFactory.java
@@ -58,7 +58,7 @@ public class JacocoModuleFactory {
     return moduleBuilder(properties)
       .mavenPlugins()
         .plugin(mavenJacocoPlugin())
-        .pluginManagement(mavenJacocoWithMinCoverageCheckPluginManagement())
+        .pluginManagement(mavenJacocoWithMinCoverageCheckPluginManagement(properties))
         .and()
       .gradlePlugins()
         .plugin(gradleJacocoWithMinCoverageCheckPlugin())
@@ -82,7 +82,8 @@ public class JacocoModuleFactory {
     return commonMavenJacocoPluginManagement().build();
   }
 
-  private static MavenPlugin mavenJacocoWithMinCoverageCheckPluginManagement() {
+  private static MavenPlugin mavenJacocoWithMinCoverageCheckPluginManagement(Seed4JModuleProperties properties) {
+    String basePackagePath = properties.basePackage().get().replace(".", "/");
     return commonMavenJacocoPluginManagement()
       .addExecution(
         pluginExecution()
@@ -91,6 +92,12 @@ public class JacocoModuleFactory {
           .configuration(
             """
               <dataFile>target/jacoco/allTest.exec</dataFile>
+              <includes>
+                <include>%s/**</include>
+              </includes>
+              <excludes>
+                <exclude>%s/**/infrastructure/secondary/**/*Entity_.class</exclude>
+              </excludes>
               <rules>
                 <rule>
                   <element>CLASS</element>
@@ -108,7 +115,7 @@ public class JacocoModuleFactory {
                   </limits>
                 </rule>
               </rules>
-            """
+            """.formatted(basePackagePath, basePackagePath)
           )
       )
       .build();

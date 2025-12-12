7fc203e1a0682d6b75079a856f0c6d3b5cc5e9ca
Quentin <quentin.monmert@gmail.com>
2025-12-12 21:52:13 +0100

    Fix: update dependency vue-tsc to v3.1.8

--- src/main/webapp/app/shared/theme-button/infrastructure/primary/ThemeButton.vue
+++ src/main/webapp/app/shared/theme-button/infrastructure/primary/ThemeButton.vue
@@ -1,20 +1,3 @@
-<template>
-  <div class="seed4j-theme-button">
-    <input
-      id="switch"
-      class="seed4j-theme-button-toggle"
-      type="checkbox"
-      name="mode"
-      :checked="!isDarkTheme"
-      data-testid="theme-switch-button"
-      @change="toggleTheme"
-    />
-    <label for="switch" class="seed4j-theme-switch-label" :aria-label="theme">
-      <span>🌙</span>
-      <span>☀️</span>
-      <div class="seed4j-theme-switch-toggle" :class="{ 'seed4j-theme-switch-toggle-checked': theme === 'dark-theme' }"></div>
-    </label>
-  </div>
-</template>
+<template src="./ThemeButton.html"></template>
 
 <script lang="ts" src="./ThemeButton.component.ts"></script>

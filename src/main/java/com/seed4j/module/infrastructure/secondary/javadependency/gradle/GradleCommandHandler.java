b6819e3712d8890c90ddddcbb36e5a304853c13a
Aurélien Mino <aurelien.mino@gmail.com>
2026-04-14 00:00:44 +0200

    feat(gradle): also declare BOM on gradle scope ANNOTATION_PROCESSING

--- src/main/java/com/seed4j/module/infrastructure/secondary/javadependency/gradle/GradleCommandHandler.java
+++ src/main/java/com/seed4j/module/infrastructure/secondary/javadependency/gradle/GradleCommandHandler.java
@@ -377,6 +377,14 @@ public class GradleCommandHandler implements JavaDependenciesCommandHandler {
       command.buildProfile().isPresent(),
       gradleDependencyScope(command.dependency())
     );
+    if (command.dependency().scope() == JavaDependencyScope.IMPORT && command.buildProfile().isEmpty()) {
+      addDependencyToBuildGradle(
+        command.dependency(),
+        buildGradleFile(Optional.empty()),
+        false,
+        GradleDependencyScope.ANNOTATION_PROCESSING
+      );
+    }
   }
 
   @Override
